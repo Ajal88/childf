f51d77697bdbcc35cfce3c2442b01228e580bba6
mina <mina.m777@yahoo.com>
2018-01-26 13:24:18 +0330

    back to sqlite

--- childf/settings.py
+++ childf/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'childf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'childf',
-        'USER': 'project',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
