4722e89aef0136349b5114fd996ef5187e4f0a48
sfaez <sfaez74@yahoo.com>
2018-01-26 13:39:27 +0330

    add view test

--- madadjoo/views.py
+++ madadjoo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def madadjoo(request):
+    #return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'madadjo.html')
