082b7c9b4a5e29310c47126a8021264cb824e9f3
mina <mina.m777@yahoo.com>
2018-02-01 22:35:31 +0330

    fix urls

--- childf/urls.py
+++ childf/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^madadjoo/', include('madadjoo.urls')),
-    url(r'^auth/', include('karbar.urls')),
+    url(r'^hamyar/', include('hamyar.urls')),
+    # url(r'^auth/', include('karbar.urls')),
 ]
