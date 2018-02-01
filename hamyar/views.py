a91bc76c1aad6dbf2c4a85c02d434b110d72deb4
Ajal88 <amirabbasjl@yahoo.com>
2018-02-01 13:56:32 +0330

    first hamyar view added

--- hamyar/views.py
+++ hamyar/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+
 
 # Create your views here.
+
+def index(Request):
+    return HttpResponse("Hello, world. You're at my hamyar index.")
