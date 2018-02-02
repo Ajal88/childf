ff099813f5a2b6fceaba66db54667bd1208fa0d4
Ajal88 <amirabbasjl@yahoo.com>
2018-02-02 11:01:34 +0330

    token check hamyar message

--- hamyar/views.py
+++ hamyar/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 from django.http import JsonResponse
 
 
 # Create your views here.
 
-def index(Request):
+def index(request):
     return HttpResponse("Hello, world. You're at my hamyar index.")
 
-def inbox()
+def inbox(request):
+    # maybe worng! TODO
+    if Karbar.objects.filter(token=request.META['HTTP_X_TOKEN']).count() == 1:
+        
