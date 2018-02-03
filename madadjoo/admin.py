2d1c994f4f3e3c880dadc953f52437a3c6373ceb
sfaez <sfaez74@yahoo.com>
2018-02-03 21:53:44 +0330

    fix madadjoo

--- madadjoo/admin.py
+++ madadjoo/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from .models import Madadjoo
+from .models import Need
 
 admin.site.register(Madadjoo)
+admin.site.register(Need)
 
--- madadjoo/views.py
+++ madadjoo/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 from django.shortcuts import render
 
-from .models import Madadjoo
+from .models import Madadjoo,Need
+
 
 
 @login_required
@@ -15,15 +16,13 @@ def madadjooHa(request):
     return render(request, 'madadjo_list.html', {'madadjooHa': c})
 
 
-''' # error in this tabee
 def madadjoo(request, username):
     c = Madadjoo.objects.get(karbar__user__username=username)
-
+    n = Need.objects.filter(madadjoo__karbar__user__username=username)
     #   messages.add_message(request, messages.SUCCESS, 'Email sent successfully.')
     # messages.add_message(request, messages.WARNING, 'You will need to change your password in one week.')
     #   messages.add_message(request, messages.INFO, 'All items on this page have free shipping.')
     #    messages.add_message(request, messages.INFO, 'All items on this page have free shipping.')
     #    messages.add_message(request, messages.INFO, 'All items on this page have free shipping.')
 
-    return render(request, 'madadjo.html', {'madadjoo': c})
-'''
+    return render(request, 'madadjo.html', {'madadjoo': c,'needs':n})
