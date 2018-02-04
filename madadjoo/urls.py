338691fd666df926899af2b31db80295a2f09615
sfaez <sfaez74@yahoo.com>
2018-02-04 20:01:15 +0330

    fix madadjoo url

--- madadjoo/urls.py
+++ madadjoo/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'signup/', views.madsignup, name='signup'),
     url(r'^list/', views.madadjooHa, name='index'),
     url(r'dashboard/(?P<username>.+)/$', views.show_dashboard, name='madadjoo_dashboard'),
-    url(r'^(?P<username>.+)/', views.madadjoo, name='index'),
+    url(r'^info/(?P<username>.+)/', views.madadjoo, name='index'),
     url(r'report_hamyar/(?P<username>.+)/$', views.report_hamyar, name='report_hamyar'),
 ]
